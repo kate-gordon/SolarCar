0da03a8d607f13b11f98526f75e63715663a5362
Chris Hill <chrishillbuilds@gmail.com>
2019-09-09 14:07:35 -0400

    updated timer and constant files

---
+++ global_constants.py
@@ -0,0 +1,4 @@
+# Constants and global variables
+
+GAME_WIDTH = 1152
+GAME_HEIGHT = 648
---
+++ timer.py
@@ -0,0 +1,25 @@
+import pygame
+from global_constants import *
+
+
+pygame.init()
+screen = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+clock = pygame.time.Clock()
+
+counter, text = 60, 'Time Left: 60'.rjust(3)
+pygame.time.set_timer(pygame.USEREVENT, 1000)
+font = pygame.font.SysFont('Consolas', 30)
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.USEREVENT: 
+            counter -= 1
+            text = "Time Left: " + str(counter).rjust(3) if counter > 0 else 'boom!'
+        if e.type == pygame.QUIT: break
+    else:
+        screen.fill((255, 255, 255))
+        screen.blit(font.render(text, True, (0, 0, 0)), (32, 48))
+        pygame.display.flip()
+        clock.tick(60)
+        continue
+    break
