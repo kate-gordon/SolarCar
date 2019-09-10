4f8c9fcdb94d28eb7c5366a7de77be29cadb3609
Kate Gordon <kategordon.code@gmail.com>
2019-09-10 14:05:10 -0400

    Kate's Working Green blocks

--- solar_Car.py
+++ solar_Car.py
@@ -1,112 +1,60 @@
 import pygame
 import random
 from global_constants import *
-from player import * 
-from timer import * 
+from pygame.locals import *
 from road_blocks import * 
-from global_constants import * 
+from player import *
 
-KEY_UP = 273
-KEY_DOWN = 274  
 
-roadblocksGroup = pygame.sprite.Group()
-roadblocksGroup.add(Roadblock)
-roadblocksGroup.add(Car)
-roadblocksGroup.add(Truck)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+FPS = 30 
+
 all_sprites = pygame.sprite.Group()
-all_sprites.add(playerGroup)
-all_sprites.add(roadblocksGroup)
-player = Player()
+aTruck = Roadblock()
+all_sprites.add(aTruck)
 
-def main():
-    width = 1152
-    height = 648
-    blue_color = (97, 159, 182)
+player = Player(185 ,GAME_HEIGHT)
+players = pygame.sprite.Group()
+players.add(player)
 
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
+pygame.init()
+pygame.mixer.init() 
+screen = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+pygame.display.set_caption("Solar Car")
+clock = pygame.time.Clock()
 
-    clock = pygame.time.Clock()
+ADDBLOCK = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDBLOCK, 1500)
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+        elif event.type == pygame.QUIT:
+            running = False
 
-    # Game initialization
-class Game(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        pygame.init()
-        pygame.display.set_caption('Solar Car')
-        self.run_intro = True
-        self.run = True
-        self.status = ''
-        self.background = ''
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            player.move_down(30)
+        if keys[pygame.K_DOWN]:
+            player.move_up(30)
 
-    def intro_screen(self):
-        self.background = solar_car_pygame.load('background_start_pygame_;').convert_alpha()
-        self.background = solar_car_pygame.transform.scale(self.background,[Game_WIDTH, Game_HEIGHT])
-        self.wait_for_screen()
+        elif(event.type == ADDBLOCK):
+            new_block = Roadblock()
+            all_sprites.add(new_block)
 
-    def Game_Over(self):
-        self.background = solar_car_pygame.image.load('background_end_GameOver').convert_alpha()
-        self.background = solar_car_pygame.transform.scale(self.background,[Game_WIDTH, Game_HEIGHT])
-        self.status = 'GAME Over'
-        self.wait_for_screen()
+    hits_block = pygame.sprite.spritecollideany(player, all_sprites)  
+    if hits_block:  
+        player.kill()
+    players.update()
+    all_sprites.update()
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    players.draw(screen)
+    pygame.display.flip()
+pygame.quit()
+   
 
-
-    def win_screen(self):
-        self.background = solar_car_pygame.image.load('pygame_you win').convert_alpha()
-        # self.background = solar_car_pygame.transfer.scale(self.background, [Game_WIDTH, Game_HEIGHT])
-        # self.status = 'Win'
-        # self.wait_for_screen()
-
-
-    def wait_for_screen(self):
-        self.screen.bilt(self.background,[0,0])
-        while self.run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-
-                if event.type == pygame.KEYUP:
-                    self.run_intro = False
-                    total.totalscore = 0
-                    self.run_game()
-    
-    def run_game(self): 
-        
-        player = Player(185, GAME_HEIGHT/2)
-        playerGroup = pygame.sprite.Group() 
-        playerGroup.add(Player)
-
-
-        pygame.time.set_timer(ADDBLOCK, 250)
-        running = True
-        while running:
-            elapsed_second = int(time.strftime("%S", time.gmtime()))
-            elapsed_minute = int(time.strftime("%M", time.gmtime()))
-            minute_timer = elapsed_minute - minute_start
-            second_timer = 60 - (elapsed_second - second_start + (minute_timer * 60))
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    running = False
-                elif event.type == pygame.QUIT:
-                    running = False
-                elif(event.type == ADDBLOCK):
-                    new_roadblock = Roadblock()
-                    roadblocksGroup.add(new_roadblock)
-                    all_sprites.add(new_enemy)
-
-        hits_block = pygame.sprite.groupcollide(player, roadblocksGroup, True) 
-        for block in hit_blocks: 
-                # timer reduced by 5 seconds
-    
-            screen.fill(blue_color)
-        playerGroup.update()
-        pygame.display.update()
-        clock.tick(60)
-
-    pygame.quit()
-
-if __name__ == '__main__':
-    game = Game()
-    game.run_game()
